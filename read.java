671b13b1fb106c47527ae2ea0ebfac4ed0eb8d08
yshnv0 <74784548+yshnv0@users.noreply.github.com>
2022-03-17 16:00:53 +0530

    Add files via upload

---
+++ read.java
@@ -0,0 +1,28 @@
+package mca;
+import java.util.*;
+
+public class read {
+
+	public static void main(String[] args) {
+		Scanner x=new Scanner(System.in);
+		System.out.println("Enter name");
+		String name=x.nextLine();
+		System.out.println("Enter Rollno");
+		int a=x.nextInt();
+		System.out.println("Enter Marks");
+		float b=x.nextFloat();
+		
+		System.out.println("DETAILS");
+		System.out.println("Name :"+name);
+		System.out.println("Roll No :"+a);
+		System.out.println("Marks :"+b);
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
